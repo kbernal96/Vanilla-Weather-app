e1d7a794822cb34cb461118b7125d753857a8b64
kbernal96 <60956525+kbernal96@users.noreply.github.com>
2021-11-21 15:03:42 -0600

    display current temperature, condition and city

--- src/script.js
+++ src/script.js
@@ -1,1 +1,22 @@
-apiKey="8d06b83f009e333b86de3e222284487f";
+
+let apiKey="18f68700369f5317f6cbee485851bf9b";
+let url=`https://api.openweathermap.org/data/2.5/find?q=New York City&units=imperial&appid=${apiKey}`;
+
+axios.get(url).then(displayTemperature);
+
+function displayTemperature(response){
+    console.log(response);
+    let weatherDescription = document.querySelector("#description");
+    let cityName = document.querySelector("#city");
+    let temperature = document.querySelector("#current-temperature");
+    let humidityCondition = document.querySelector("#humidity");
+    let windSpeed = document.querySelector("#wind");
+    let feelsLike = document.querySelector("#feels-like");
+
+    weatherDescription.innerHTML = response.data.list[0].weather[0].description;
+    cityName.innerHTML = response.data.list[0].name;
+    temperature.innerHTML = Math.round(response.data.list[0].main.temp);
+    humidityCondition.innerHTML = response.data.list[0].main.humidity;
+    windSpeed.innerHTML = Math.round(response.data.list[0].wind.speed);
+    feelsLike.innerHTML = Math.round(response.data.list[0].main.feels_like);
+}
