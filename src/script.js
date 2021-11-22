79baf94fc0d63397d6abec91161ef7185f6a391c
kbernal96 <60956525+kbernal96@users.noreply.github.com>
2021-11-22 13:00:01 -0600

    added timestamp function

--- src/script.js
+++ src/script.js
@@ -1,17 +1,20 @@
 
 let apiKey="18f68700369f5317f6cbee485851bf9b";
+//let city ="";
 let url=`https://api.openweathermap.org/data/2.5/find?q=New York City&units=imperial&appid=${apiKey}`;
 
 axios.get(url).then(displayTemperature);
 
 function displayTemperature(response){
     console.log(response);
-    let weatherDescription = document.querySelector("#description");
+    let weatherDescription = document.querySelector("#weather-description");
     let cityName = document.querySelector("#city");
     let temperature = document.querySelector("#current-temperature");
     let humidityCondition = document.querySelector("#humidity");
     let windSpeed = document.querySelector("#wind");
     let feelsLike = document.querySelector("#feels-like");
+    let date = document.querySelector("#current-date");
+    let icon = document.querySelector("#weather-icon");
 
     weatherDescription.innerHTML = response.data.list[0].weather[0].description;
     cityName.innerHTML = response.data.list[0].name;
@@ -19,4 +22,28 @@ function displayTemperature(response){
     humidityCondition.innerHTML = response.data.list[0].main.humidity;
     windSpeed.innerHTML = Math.round(response.data.list[0].wind.speed);
     feelsLike.innerHTML = Math.round(response.data.list[0].main.feels_like);
+    date.innerHTML = formatDate(response.data.list[0].dt * 1000);
+    icon.setAttribute("src", "")
+}
+
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday0", "Saturday"];
+
+    day = day[date.getDay()];
+    
+
+    if (hours < 10){
+        hours = `0${hours}`;
+    }
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day}, ${hours}:${minutes}`;
+
+    
 }
